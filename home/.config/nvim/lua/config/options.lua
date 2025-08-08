57bb6fcd9d195e3d928be5198fee469932774440
Etienne van Delden de la Haije <etienne@vandelden.family>
2025-08-09 00:20:21 +0200

    Better defauls for lazyvim: wrap at end of panel, always add a blank link, set cursor to 2 different types

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,5 +1,24 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+-- Enable wrap
+opt.wrap = true
+
+-- Always add a blank line at the end
+opt.endofline = true
+opt.fixendofline = true
+opt.fixeol = true
+
+-- set cursor
+opt.guicursor = {
+  -- Normal/visual/command: thick solid bar
+  "n-v-c:hor70",
+
+  -- Insert/replace/etc: thin blinking bar
+  "i-ci-ve-r-cr-sm:ver20-blinkwait300-blinkon200-blinkoff150",
+}
+
+-- RUBY
 vim.g.lazyvim_ruby_lsp = "solargraph"
 vim.g.lazyvim_ruby_formatter = "rubocop"
