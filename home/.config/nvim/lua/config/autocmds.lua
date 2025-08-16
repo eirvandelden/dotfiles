9fb8502904b052e74fa3593529ebb3812ba361eb
Etienne van Delden de la Haije <etienne@vandelden.family>
2025-08-16 22:17:28 +0200

    Disable autoformatting of lua.erb files

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+--Disable autoformat for ERB-wrapped Lua files
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+  pattern = "*.lua.erb",
+  callback = function(args)
+    -- LazyVim checks this flag before formatting
+    vim.b[args.buf].autoformat = false
+  end,
+})
+
+-- Also catch buffers that change filetype later
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "eruby", "eruby.lua" }, -- depending on your ft detection
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
