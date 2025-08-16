a50d20a591f019cebb956d234a026a4ceda688d5
Etienne van Delden de la Haije <etienne@vandelden.family>
2025-08-16 22:19:34 +0200

    Add smear cursor while jumping

---
+++ home/.config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,9 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    vertical_bar_cursor = true,
+    vertical_bar_cursor_insert_mode = true,
+    horizontal_bar_cursor_replace_mode = true,
+    cursor_color = "#0000FF",
+  },
+}
